645b56a26e95dcf44ee0aaa062df00df8edf291d
DAKSH KAUSHAL <129703289+OVERFORGE@users.noreply.github.com>
2025-01-26 23:44:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import ReactDOM from "react-dom";
 import Category from "./pages/category";
 import Footer from "./components/Footer";
